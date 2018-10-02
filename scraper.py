2898096c0a53e5cc60a327a28b6951bd6fd4ea5f
tudor <tudor.sicaru@gmail.com>
2018-10-02 08:49:59 +0300

    Added print messages for verbosity.

--- scraper.py
+++ scraper.py
@@ -11,10 +11,13 @@ from urllib.request import urlopen as get
 
 url = 'https://coinmarketcap.com/all/views/all/'
 retlist = []
+outfile = 'test.csv'
 header = ['coin_name', 'market_cap', 'price', 'price_change']
 
 with get(url) as u_page:
+    print(f"Connecting to: '{url}'")
     html_page = u_page.read()
+    print('Parsing the retrieved web page')
     parsed_page = soup(html_page, 'html.parser')
     table = parsed_page.findAll('table', {'id': 'currencies-all'})
     t_rows = table[0].tbody.findAll('tr')
@@ -28,9 +31,9 @@ for tr in t_rows:
         table_list.append(tr.find('td', {'data-timespan': '7d'}).text.strip('%'))
     except Exception:
         continue
-    cur_dict = {k: v for k, v in zip(header, table_list)}
-    retlist.append(cur_dict)
+    retlist.append({k: v for k, v in zip(header, table_list)})
 
-with DictToCsvWriter('test.csv', header=header) as writer:
+print(f"Writing the scraped data to '{outfile}'")
+with DictToCsvWriter(outfile, header=header) as writer:
     writer.add_lines(retlist)
     writer.write()
